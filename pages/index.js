2bc8563ee417940fcc5d1ae991bbc37a620dc171
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-01-26 21:31:53 -0300

    Demonstração de como adicionar um estilo ao componente

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+// Componente React
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    // JSX
+    return (
+        <div>
+            <h1>Bem vindo de volta</h1>
+            <h2>Discord - Alura MAtrix</h2>
+
+            <style jsx>{`
+                h1 {
+                    color: red;
+                }
+            `}</style>
+        </div>
+    )   
 }
 
 export default HomePage
