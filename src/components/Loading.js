46cdd8fb8680898d71fc363a7517e49a1499fe0a
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-02-02 14:04:18 -0300

    Loading tem propriedade de cor

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -9,6 +9,7 @@ import {Events} from "../Events.js";
 export default function Loading(props) {
 
     const [loading, setLoading] = React.useState(false);
+    const color = props.color || "secondary";
 
     React.useEffect(() => {
         eventBus.on(Events.START_LOADING, () => {
@@ -22,11 +23,10 @@ export default function Loading(props) {
         });
 
         return () => {
-            console.log("Removendo os ouvintes do Loading")
             eventBus.remove(Events.START_LOADING);
             eventBus.remove(Events.STOP_LOADING);
         };
     }, []);
 
-    return <Box>{loading ? <CircularProgress color="primary" /> : ""}</Box>;
+    return <Box>{loading ? <CircularProgress color={color} /> : ""}</Box>;
 }
